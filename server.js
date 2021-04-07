a3284055808ab701014b09adeeb477907e035893
kartikchechani <chechanikartik@gmail.com>
2021-04-07 12:28:04 +0530

    changes

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+
+var io = require('socket.io')(process.env.PORT || 3000);
 
 var port = process.env.PORT || 3000
 
